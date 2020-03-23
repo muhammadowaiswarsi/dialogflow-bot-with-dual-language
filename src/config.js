957084e2dd3b7c4e498e09d2236fc95f23fba73f
Muhammad Owais Warsi <owaiskhan148@gmail.com>
2020-03-23 16:25:26 +0500

    update config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
     app: {
-        gateway: 'https://open-mensa-bot-jesvfd.core.ushaflow.com', // <- enter your gateway URL here
+        gateway: 'https://mensa-chatbot-eng-tqwxnq.core.ushaflow.com', // <- enter your gateway URL here
         muted: false, // <- mute microphone at start
         start_suggestions: [], // <- array of suggestions, displayed at the start screen
         fallback_lang: 'en', // <- fallback language code, if history mode or network is unavailable,
